11af8a005835d81496fca13842bae4e1edf821d3
sabita rajbanshi <sabitarajbanshi.tech@gmail.com>
2021-05-30 03:09:09 +0545

    stage 1 added

--- src/get_data.py
+++ src/get_data.py
@@ -18,7 +18,7 @@ def get_data(config_path):
     df = pd.read_csv(data_path, sep=",", encoding='utf-8')
     #print(df.head())
     return df
-
+    
 if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
