46a1e59bb3e5d13ba2d3d8615f83969c72a00637
sabita rajbanshi <sabitarajbanshi.tech@gmail.com>
2021-05-30 15:45:04 +0545

    setup done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages, version
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="it is a wineq package",
+    author="r-sabita",
+    packages=find_packages(),
+    license="MIT"
+
+)
+
