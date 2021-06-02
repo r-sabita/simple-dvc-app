03bc3897cb0c263b878b86c1428774578f84cd84
sabita rajbanshi <sabitarajbanshi.tech@gmail.com>
2021-06-02 18:46:38 +0545

    stage 2 completed

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -14,7 +14,7 @@ import argparse
 import joblib
 import json
 
-
+ 
 def eval_metrics(actual, pred):
     rmse = np.sqrt(mean_squared_error(actual, pred))
     mae = mean_absolute_error(actual, pred)
